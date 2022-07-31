441daa8906abf33a1d51ce3fd9113bb190575a7d
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2022-07-30 22:50:15 -0400

    askbot/startup_procedures.py: corrects message about latest supported version of Python

--- askbot/startup_procedures.py
+++ askbot/startup_procedures.py
@@ -986,9 +986,9 @@ def test_versions():
     if dj_ver < (2, 2) or dj_ver >= (4, 0):
         errors.append('This version of Askbot supports django 2.2 - 3.2 ' + upgrade_msg)
     elif py_ver[:3] < (3, 6, 0):
-        errors.append('Askbot requires Python 3.6 - 3.9')
+        errors.append('Askbot requires Python 3.6 - 3.10')
     elif py_ver[:3] > (3, 11, 0):
-        errors.append("""Askbot was not tested with Python > 3.9
+        errors.append("""Askbot was not tested with Python > 3.10.x
 Try adding ASKBOT_SELF_TEST = False to the settings.py
 to test if your version of Python works and please let us know.""")
 
