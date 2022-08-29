06183b9f2c7f7fa9d11f98ad69ad3597da5beb1a
Skyler Mäntysaari <samip5@users.noreply.github.com>
2022-08-29 16:06:11 +0300

    management/commands/askbot_add_osqa_content: Reformat code according to PEP8

--- askbot/management/commands/askbot_add_osqa_content.py
+++ askbot/management/commands/askbot_add_osqa_content.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 from django.db.models import Q
 from django.utils import translation
 from django.conf import settings as django_settings
-from django.utils.http import urlquote  as django_urlquote
+from django.utils.http import urlquote as django_urlquote
 from django.utils import timezone
 from django.template.defaultfilters import slugify
 from askbot.deps.django_authopenid.models import UserAssociation
@@ -19,6 +19,7 @@ from askbot.models import Tag
 from askbot.models import User
 from askbot.utils.slug import slugify_camelcase
 from askbot import const
+
 
 def decode_datetime(data):
     """Decodes formats:
@@ -34,6 +35,7 @@ def decode_datetime(data):
             except ValueError:
                 return datetime.strptime(data, '%Y-%m-%d')
     return None
+
 
 class DataObject:
     def __init__(self, soup):
@@ -86,7 +88,6 @@ class DataObject:
             return self.decode_rel_value(field)
         raise ValueError('unknown field class - neither data nor relation')
 
-
     def __getattr__(self, key):
         """Returns value of property, if decoded
         or decodes the property first from the bs4 soup"""
@@ -109,48 +110,48 @@ class Command(BaseImportXMLCommand):
         xml = open(dump_file_name, 'r').read()
         self.soup = BeautifulSoup(xml, ['lxml', 'xml', 'lxml-xml'])
 
-        #site settings
-        #forum.keyvalue
+        # site settings
+        # forum.keyvalue
         self.import_users()
         self.import_user_logins()
-        #model="forum.tag"
+        # model="forum.tag"
         self.import_tags()
 
-        #model="forum.question"/answer/comment - derivatives of the Node model
+        # model="forum.question"/answer/comment - derivatives of the Node model
         self.import_threads()
         self.import_posts('question', True)
-        #inside we also mark accepted answer, b/c it's more convenient that way
+        # inside we also mark accepted answer, b/c it's more convenient that way
         self.import_posts('answer')
         self.import_posts('comment')
-        #model="forum.noderevision"
+        # model="forum.noderevision"
         self.import_post_revisions()
 
         self.fix_answer_counts()
         self.fix_comment_counts()
 
-        #model="forum.subscriptionsettings"
-        #this model has no correspondence in Askbot
+        # model="forum.subscriptionsettings"
+        # this model has no correspondence in Askbot
 
-        #model="forum.actionrepute"
-        #model="forum.award"
+        # model="forum.actionrepute"
+        # model="forum.award"
 
-        #model="forum.nodestate"
+        # model="forum.nodestate"
 
-        #model="forum.question"
-        #model="forum.questionsubscription"
-        #model="forum.userproperty"
-        #model="forum.validationhash"
-        #model="forum.vote"
+        # model="forum.question"
+        # model="forum.questionsubscription"
+        # model="forum.userproperty"
+        # model="forum.validationhash"
+        # model="forum.vote"
 
-        #self.import_marked_tags()
+        # self.import_marked_tags()
 
-        #self.apply_groups_to_threads()
+        # self.apply_groups_to_threads()
 
-        #self.apply_question_followers()
+        # self.apply_question_followers()
         self.import_votes()
 
         self.import_badges()
-        #self.import_badge_awards()
+        # self.import_badge_awards()
 
     def get_objects_for_model(self, model_name):
         objects_soup = self.soup.find_all(attrs={'model': model_name})
@@ -159,9 +160,9 @@ class Command(BaseImportXMLCommand):
 
     def import_users(self):
         """import OSQA users to Askbot users"""
-        #in OSQA user profile is split in two models
-        #auth.user
-        #forum.user
+        # in OSQA user profile is split in two models
+        # auth.user
+        # forum.user
         for from_user in self.get_objects_for_model('auth.user'):
             try:
                 to_user = User.objects.get(email=from_user.email)
@@ -202,7 +203,7 @@ class Command(BaseImportXMLCommand):
             try:
                 assoc = UserAssociation()
                 assoc.openid_url = user_login.key
-                assoc.user =  self.get_imported_object_by_old_id(User, user_login.user)
+                assoc.user = self.get_imported_object_by_old_id(User, user_login.user)
                 assoc.provider_name = user_login.provider
                 assoc.last_used_timestamp = user_login.added_at
             except django.db.utils.IntegrityError as e:
@@ -240,7 +241,7 @@ class Command(BaseImportXMLCommand):
 
     def import_badges(self):
         """remembers relation of OSQA badges with Askbot badges"""
-        #model="forum.badge"
+        # model="forum.badge"
         for osqa_badge in self.get_objects_for_model('forum.badge'):
             badge_slug = slugify_camelcase(osqa_badge.cls)
             try:
@@ -279,17 +280,17 @@ class Command(BaseImportXMLCommand):
             if badge is None:
                 continue
             print(f'awarding badge {badge.slug}')
-            #if multiple or user does not have this badge, then award
+            # if multiple or user does not have this badge, then award
             if badge.is_multiple() or (not user.has_badge(badge)):
                 award = Award()
                 award.badge = badge
                 award.user = user
                 award.notified = True
-                #todo: here we need to map to the node object
-                #content_type = self.get_content_type_by_old_id(award.content_type_id)
-                #obj_class = content_type.model_class()
-                #award.object_id = self.get_imported_object_id_by_old_id(obj_class, award.object_id)
-                #award.content_type = content_type
+                # todo: here we need to map to the node object
+                # content_type = self.get_content_type_by_old_id(award.content_type_id)
+                # obj_class = content_type.model_class()
+                # award.object_id = self.get_imported_object_id_by_old_id(obj_class, award.object_id)
+                # award.content_type = content_type
                 award.save()
             """
             <object model="forum.award" pk="1">
@@ -319,31 +320,31 @@ class Command(BaseImportXMLCommand):
         count = 0
         for osqa_thread in self.get_objects_for_model('forum.question'):
             count += 1
-            #todo: there must be code lated to set the commented values
+            # todo: there must be code lated to set the commented values
             lang = django_settings.LANGUAGE_CODE
             last_actor = self.get_imported_object_by_old_id(User, osqa_thread.last_activity_by)
             thread = Thread(
                 title=osqa_thread.title,
                 tagnames=osqa_thread.tagnames,
                 view_count=osqa_thread.extra_count,
-                #favourite_count=thread.favourite_count,
-                #answer_count=thread.answer_count,
+                # favourite_count=thread.favourite_count,
+                # answer_count=thread.answer_count,
                 last_activity_at=osqa_thread.last_activity_at,
                 last_activity_by=last_actor,
                 language_code=lang,
-                #"closed" data is stored differently in OSQA
-                #closed_by=self.get_imported_object_by_old_id(User, thread.closed_by_id),
-                #closed=thread.closed,
-                #closed_at=thread.closed_at,
-                #close_reason=thread.close_reason,
-                #deleted=False,
-                approved=True, #no equivalent in OSQA
-                #must be done later, after importing answers
-                #answer_accepted_at=thread.answer_accepted_at,
+                # "closed" data is stored differently in OSQA
+                # closed_by=self.get_imported_object_by_old_id(User, thread.closed_by_id),
+                # closed=thread.closed,
+                # closed_at=thread.closed_at,
+                # close_reason=thread.close_reason,
+                # deleted=False,
+                approved=True,  # no equivalent in OSQA
+                # must be done later, after importing answers
+                # answer_accepted_at=thread.answer_accepted_at,
                 added_at=osqa_thread.added_at,
             )
 
-            #apply tags to threads
+            # apply tags to threads
             tag_names = thread.get_tag_names()
             if tag_names:
 
@@ -380,46 +381,46 @@ class Command(BaseImportXMLCommand):
 
         for osqa_node in self.get_objects_for_model(model_name):
             try:
-                #we iterate through all nodes, but pick only the ones we need
+                # we iterate through all nodes, but pick only the ones we need
                 if osqa_node.node_type != post_type:
                     continue
 
-                #cheat: do not import deleted content
+                # cheat: do not import deleted content
                 if '(deleted)' in osqa_node.state_string:
                     continue
 
                 post = Post()
 
-                #this line is a bit risky, but should work if we import things in correct order
+                # this line is a bit risky, but should work if we import things in correct order
                 if osqa_node.parent:
                     post.parent = self.get_imported_object_by_old_id(Post, osqa_node.parent)
                     if post.parent is None:
-                        continue #deleted parent
+                        continue  # deleted parent
                     post.thread = post.parent.thread
                 else:
                     post.thread = self.get_imported_object_by_old_id(Thread, osqa_node.id)
                     if post.thread is None:
-                        continue #deleted thread
+                        continue  # deleted thread
 
                 post.post_type = osqa_node.node_type
                 post.added_at = osqa_node.added_at
 
                 if save_redirects:
                     slug = django_urlquote(slugify(osqa_node.title))
-                    #todo: add i18n to the old url
+                    # todo: add i18n to the old url
                     old_url = f'/questions/{osqa_node.id}/{slug}/'
 
                 post.author = self.get_imported_object_by_old_id(User, osqa_node.author)
-                #html will de added with the revisions
-                #post.html = HTMLParser().unescape(osqa_node.body)
+                # html will de added with the revisions
+                # post.html = HTMLParser().unescape(osqa_node.body)
                 post.summary = post.get_snippet()
 
-                #these don't have direct equivalent in the OSQA Node object
-                #post.deleted_by - deleted nodes are not imported
-                #post.locked_by
-                #post.last_edited_by
+                # these don't have direct equivalent in the OSQA Node object
+                # post.deleted_by - deleted nodes are not imported
+                # post.locked_by
+                # post.last_edited_by
 
-                #these are to be set later with the real values
+                # these are to be set later with the real values
                 post.points = 0
                 post.vote_up_count = 0
                 post.vote_down_count = 0
@@ -427,7 +428,7 @@ class Command(BaseImportXMLCommand):
 
                 post.save()
 
-                #mark accepted answer
+                # mark accepted answer
                 now = timezone.now()
                 if osqa_node.node_type == 'answer':
                     if '(accepted)' in osqa_node.state_string:
@@ -435,7 +436,6 @@ class Command(BaseImportXMLCommand):
                         post.endorsed = True
                         post.endorsed_at = now
                         post.thread.save()
-
 
                 if save_redirects:
                     new_url = post.get_absolute_url()
@@ -455,18 +455,18 @@ class Command(BaseImportXMLCommand):
         for osqa_revision in self.get_objects_for_model('forum.noderevision'):
             post = self.get_imported_object_by_old_id(Post, osqa_revision.node)
             if post is None:
-                continue #deleted post
+                continue  # deleted post
             user = self.get_imported_object_by_old_id(User, osqa_revision.author)
             revision = PostRevision(
-                            post=post,
-                            author=user,
-                            text=osqa_revision.body,
-                            title=osqa_revision.title,
-                            tagnames=osqa_revision.tagnames,
-                            revised_at=osqa_revision.revised_at,
-                            summary=osqa_revision.summary,
-                            revision=osqa_revision.revision
-                        )
+                post=post,
+                author=user,
+                text=osqa_revision.body,
+                title=osqa_revision.title,
+                tagnames=osqa_revision.tagnames,
+                revised_at=osqa_revision.revised_at,
+                summary=osqa_revision.summary,
+                revision=osqa_revision.revision
+            )
             post.text = osqa_revision.body
             if osqa_revision == 1:
                 post.added_at = osqa_revision.revised_at
@@ -482,7 +482,7 @@ class Command(BaseImportXMLCommand):
         for osqa_vote in self.get_objects_for_model('forum.vote'):
             post = self.get_imported_object_by_old_id(Post, osqa_vote.node)
             if post is None:
-                continue #deleted post
+                continue  # deleted post
             user = self.get_imported_object_by_old_id(User, osqa_vote.user)
             if osqa_vote.value > 0:
                 user.upvote(post, timestamp=osqa_vote.voted_at, force=True)
